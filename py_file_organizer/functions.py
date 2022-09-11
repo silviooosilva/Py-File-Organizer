bb9e684fd68fec9c5c99aedd344f1909a17e2e3f
Marcos Lima <marcos.j.s.lima@gmail.com>
2022-09-10 21:10:13 -0300

    Código movido para package py_file_organizer Criados testes unitários, refatorando conforme necessário.

---
+++ py_file_organizer/functions.py
@@ -0,0 +1,103 @@
+"""
+By: Sílvio Silva
+   07/09/2022
+"""
+
+import os
+import shutil
+from os import listdir
+from os.path import isfile, join
+from colorama import Fore
+
+from py_file_organizer.extensions import extensions
+
+
+class PyFileOrganizer:
+    def __init__(self, directory: str, dryrun: bool = True):
+        self.dryrun = dryrun
+        self.directory = directory
+
+    def _check_dir(self):
+        if not os.path.isdir(self.directory):
+            print(f'{Fore.RED} The Directory {self.directory} '
+                  f'Does not exist. Try Again [!]')
+            return False
+        return True
+
+    def run(self):
+        if not self._check_dir():
+            return
+        self._create_organization_dirs()
+        self._order()
+
+    @staticmethod
+    def get_extension(path: str):
+        """
+        Função para pegar as extensões dos arquivos
+        """
+        return os.path.splitext(path)[1].lower()
+
+    def _create_dir(self, directory: str):
+        """
+        Função para criar os diretórios onde serão organizados os arquivos
+        """
+        try:
+            os.chdir(self.directory)
+            if not os.path.isdir(directory):
+                if self.dryrun:
+                    print(f'Creating directory {directory}')
+                else:
+                    os.mkdir(directory)
+        except Exception:
+            pass
+
+    def _move(self, arg1, arg2):
+        if self.dryrun:
+            print(f"DRYRUN: {arg1} -> {arg2}")
+            return True
+        else:
+            return shutil.move(arg1, arg2)
+
+    def _order(self):
+        onlyfiles = self._get_files()
+        tdd = self._process_files(onlyfiles)
+        self._show_result_message(tdd)
+
+    def _process_files(self, onlyfiles):
+        tdd = 0
+        for arquivo in onlyfiles:
+            tdd += self._process_file(arquivo)
+        return tdd
+
+    @staticmethod
+    def _show_result_message(tdd):
+        if tdd > 0:
+            print(f"{Fore.GREEN}Organization made with success[!]")
+        else:
+            print(f"{Fore.YELLOW}This dir is already organizaded[!]")
+
+    def _process_file(self, arquivo):
+        extension = self.get_extension(arquivo)
+        target_dir = self._get_target_dir(extension)
+        if target_dir is None or os.path.isfile(f"{target_dir}"):
+            return 0
+
+        if self._move(arquivo, target_dir):
+            return 1
+        return 0
+
+    @staticmethod
+    def _get_target_dir(extension):
+        for dirname, exts in extensions.items():
+            if extension in exts:
+                return dirname
+        return None
+
+    def _get_files(self):
+        onlyfiles = [f for f in listdir(self.directory)
+                     if isfile(join(self.directory, f))]
+        return onlyfiles
+
+    def _create_organization_dirs(self):
+        for dirname, exts in extensions.items():
+            self._create_dir(dirname)
--- py_file_organizer/main.py
+++ py_file_organizer/main.py
@@ -1,11 +1,12 @@
 from colorama import Fore
+# noinspection PyPackageRequirements
 from consolemenu import ConsoleMenu
+# noinspection PyPackageRequirements
 from consolemenu.items import FunctionItem
+from py_file_organizer.functions import PyFileOrganizer
 
-from functions import order
 
-
-class PyFileOrganizer:
+class PyFileOrganizerUI:
     def __init__(self):
         pass
 
@@ -17,16 +18,23 @@ class PyFileOrganizer:
         menu = ConsoleMenu(f"{Fore.WHITE}PyFileOrganizer",
                            "Organize your files")
 
+        self._add_start_item(menu)
+        self._add_info_item(menu)
+
+        menu.show()
+
+    def _add_info_item(self, menu):
+        info_item = FunctionItem("Info", self.on_info)
+        menu.append_item(info_item)
+
+    def _add_start_item(self, menu):
         start_item = FunctionItem("Start App", self.on_start_app)
         menu.append_item(start_item)
 
-        info_item = FunctionItem("Info", self.on_info)
-        menu.append_item(info_item)
-        menu.show()
-
     def on_start_app(self):
         path = str(input('Paste the directory location: '))
-        order(path)
+        organizer = PyFileOrganizer(path)
+        organizer.run()
         self._wait_user()
 
     def on_info(self):
@@ -34,8 +42,12 @@ class PyFileOrganizer:
         self._wait_user()
 
 
-if __name__ == '__main__':
-    app = PyFileOrganizer()
+def main():
+    app = PyFileOrganizerUI()
     app.run()
     print("[!] Enjoy :) Bye")
     print(f'{Fore.YELLOW}By: Sílvio Silva')
+
+
+if __name__ == '__main__':
+    main()
